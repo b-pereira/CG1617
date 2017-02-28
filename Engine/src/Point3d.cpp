66ccfc7dcf37d4563df080612bdc1627ac9ad49f
_bpp <pereirabruno05@gmail.com>
2017-02-28 11:25:06 +0000

    Adiciona classe para ponto 3D

---
+++ Engine/src/Point3d.cpp
@@ -0,0 +1,45 @@
+/*
+ * Point3d.cpp
+ *
+ *  Created on: 26/02/2017
+ *      Author: bpereira
+ */
+
+#include "Point3d.h"
+
+Point3d::Point3d() :
+		x_coord(0), y_coord(0), z_coord(0) {
+}
+
+Point3d::Point3d(double x, double y, double z) :
+		x_coord(x), y_coord (y), z_coord (z) {
+}
+
+double Point3d::getXCoord() const {
+	return x_coord;
+}
+
+void Point3d::setXCoord(double coord) {
+	x_coord = coord;
+}
+
+double Point3d::getYCoord() const {
+	return y_coord;
+}
+
+void Point3d::setYCoord(double coord) {
+	y_coord = coord;
+}
+
+double Point3d::getZCoord() const {
+	return z_coord;
+}
+
+void Point3d::setZCoord(double coord) {
+	z_coord = coord;
+}
+
+Point3d::~Point3d() {
+	// TODO Auto-generated destructor stub
+}
+
