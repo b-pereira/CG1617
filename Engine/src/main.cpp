c2f5021e36201719ae7ffc4b6ea8534db3d5b868
Bruno Pereira <pereirabruno05@gmail.com>
2017-03-06 14:27:34 +0000

    Remove comentários de produção

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -30,270 +30,292 @@ float alpha = 0, beta = 0, raio = 10;
 //
 //std::map <string, vector<Triangle>>::iterator it=dict.begin();
 
-void changeSize(int w, int h) {
+void
+changeSize (int w, int h)
+{
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if (h == 0)
-		h = 1;
+  // Prevent a divide by zero, when window is too short
+  // (you cant make a window with zero width).
+  if (h == 0)
+    h = 1;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
+  // compute window's aspect ratio
+  float ratio = w * 1.0 / h;
 
-	// Set the projection matrix as current
-	glMatrixMode(GL_PROJECTION);
-	// Load Identity Matrix
-	glLoadIdentity();
+  // Set the projection matrix as current
+  glMatrixMode (GL_PROJECTION);
+  // Load Identity Matrix
+  glLoadIdentity ();
 
-	// Set the viewport to be the entire window
-	glViewport(0, 0, w, h);
+  // Set the viewport to be the entire window
+  glViewport (0, 0, w, h);
 
-	// Set perspective
-	gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+  // Set perspective
+  gluPerspective (45.0f, ratio, 1.0f, 1000.0f);
 
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
+  // return to the model view matrix mode
+  glMatrixMode (GL_MODELVIEW);
 }
 
-void renderScene(void) {
+void
+renderScene (void)
+{
 
-	// clear buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  // clear buffers
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// set the camera
-	glLoadIdentity();
-	/**
-	 * Definição da camara anterior
-	 */
-	//gluLookAt(0.0, 10.0, 25.0, 0.0, 0.0, 0.0, 0.0f, 1.0f, 0.0f);
-	gluLookAt(
-			cos(beta)*sin(alpha)*raio, sin(beta)*raio, cos(beta)*cos(alpha)*raio, //  coordenadas esfericas inicialmente (0,0,1)
-			0, 0, 0,
-			0, 1, 0);
-	//gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0f, 1.0f, 0.0f);
+  // set the camera
+  glLoadIdentity ();
+
+
+  gluLookAt (cos (beta) * sin (alpha) * raio, sin (beta) * raio,
+	     cos (beta) * cos (alpha) * raio, //  coordenadas esfericas inicialmente (0,0,1)
+	     0, 0, 0, 0, 1, 0);
+
 
 // put the geometric transformations here
-	glTranslatef(mover_x, 0.0, mover_z);
+  glTranslatef (mover_x, 0.0, mover_z);
 
-	glRotatef(theta, 0.0f, 1.0f, 0.0f);
+  glRotatef (theta, 0.0f, 1.0f, 0.0f);
 
-	glRotatef(phi, 1.0f, 0.0f, 0.0f);
+  glRotatef (phi, 1.0f, 0.0f, 0.0f);
 
-	// put drawing instructions here
-	glBegin(GL_TRIANGLES);
-	//lados
+  // put drawing instructions here
+  glBegin (GL_TRIANGLES);
+  //lados
 
-	/** TODO: Para já só está a ler da primeira entrada do ficheiro XML */
-	/** TODO: Trabalhar iterador */
-	for (Triangle t : figures.at(idx)) {
+  for (Triangle t : figures.at (idx))
+    {
 
-		Point3d a = t.getA();
-		Point3d b = t.getB();
-		Point3d c = t.getC();
+      Point3d a = t.getA ();
+      Point3d b = t.getB ();
+      Point3d c = t.getC ();
 
-		glVertex3f(a.getXCoord(), a.getYCoord(), a.getZCoord());
+      glVertex3f (a.getXCoord (), a.getYCoord (), a.getZCoord ());
 
-		glVertex3f(b.getXCoord(), b.getYCoord(), b.getZCoord());
+      glVertex3f (b.getXCoord (), b.getYCoord (), b.getZCoord ());
 
-		//ponto A
-		glVertex3f(c.getXCoord(), c.getYCoord(), c.getZCoord());
+      //ponto A
+      glVertex3f (c.getXCoord (), c.getYCoord (), c.getZCoord ());
 
-		glColor3f(1, 1, 0);
+      glColor3f (1, 1, 0);
 
-	}
+    }
 
-	glEnd();
+  glEnd ();
 
-	// End of frame
-	glutSwapBuffers();
+  // End of frame
+  glutSwapBuffers ();
 }
 
 // write function to process keyboard events
-void keyboardR(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'a':
-		mover_x = mover_x - 0.1;
-		break;
-	case 'd':
-		mover_x = mover_x + 0.1;
-		break;
-	case 's':
-		mover_z = mover_z + 0.1;
-		break;
-	case 'w':
-		mover_z = mover_z - 0.1;
-		break;
-	case 'r':
-		mover_z = 0;
-		mover_x = 0;
-		phi = 0;
-		theta = 0;
-		break;
-	case 'n':
-		//++it;
+void
+keyboardR (unsigned char key, int x, int y)
+{
+  switch (key)
+    {
+    case 'a':
+      mover_x = mover_x - 0.1;
+      break;
+    case 'd':
+      mover_x = mover_x + 0.1;
+      break;
+    case 's':
+      mover_z = mover_z + 0.1;
+      break;
+    case 'w':
+      mover_z = mover_z - 0.1;
+      break;
+    case 'r':
+      mover_z = 0;
+      mover_x = 0;
+      phi = 0;
+      theta = 0;
+      idx = 0;
+      break;
+    case 'n':
 
-		idx = (idx + 1) % figures.size();
+      idx = (idx + 1) % figures.size ();
 
-		break;
-	case 'h':
-		alpha -= 0.1;
-		break;
-	case 'l':
-		alpha += 0.1;
-		break;
-	case 'k':
-		if (beta < M_PI / 2)
-			beta += 0.1;
-		break;
-	case 'j':
-		if (beta > -M_PI / 2)
-			beta -= 0.1;
-		break;
+      break;
+    case 'h':
+      alpha -= 0.1;
+      break;
+    case 'l':
+      alpha += 0.1;
+      break;
+    case 'k':
+      if (beta < M_PI / 2)
+	beta += 0.1;
+      break;
+    case 'j':
+      if (beta > -M_PI / 2)
+	beta -= 0.1;
+      break;
 
-	default:
-		break;
-	}
-	glutPostRedisplay();
+    default:
+      break;
+    }
+  glutPostRedisplay ();
 }
 
-void keyboardS(int key_code, int x, int y) {
-	switch (key_code) {
-	case GLUT_KEY_LEFT:
-		theta = theta - 5.0;
+void
+keyboardS (int key_code, int x, int y)
+{
+  switch (key_code)
+    {
+    case GLUT_KEY_LEFT:
+      theta = theta - 5.0;
 
-		break;
-	case GLUT_KEY_RIGHT:
-		theta = theta + 5.0;
+      break;
+    case GLUT_KEY_RIGHT:
+      theta = theta + 5.0;
 
-		break;
-	case GLUT_KEY_UP:
-		phi = phi + 5.0;
+      break;
+    case GLUT_KEY_UP:
+      phi = phi + 5.0;
 
-		break;
-	case GLUT_KEY_DOWN:
-		phi = phi - 5.0;
+      break;
+    case GLUT_KEY_DOWN:
+      phi = phi - 5.0;
 
-		break;
+      break;
 
-	default:
-		break;
-	}
-	glutPostRedisplay();
+    default:
+      break;
+    }
+  glutPostRedisplay ();
 }
 
-void menu(int op) {
-	switch (op) {
-	case 1:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-		break;
-	case 2:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		break;
-	case 3:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		break;
-	default:
-		break;
-	}
-	glutPostRedisplay();
+void
+menu (int op)
+{
+  switch (op)
+    {
+    case 1:
+      glPolygonMode (GL_FRONT_AND_BACK, GL_POINT);
+      break;
+    case 2:
+      glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
+      break;
+    case 3:
+      glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+      break;
+    default:
+      break;
+    }
+  glutPostRedisplay ();
 }
 
-const vector<string> explode(const string& s, const char& c) {
-	string buff { "" };
-	vector<string> v;
+const vector<string>
+explode (const string& s, const char& c)
+{
+  string buff
+    { "" };
+  vector<string> v;
 
-	for (auto n : s) {
-		if (n != c)
-			buff += n;
-		else if (n == c && buff != "") {
-			v.push_back(buff);
-			buff = "";
-		}
+  for (auto n : s)
+    {
+      if (n != c)
+	buff += n;
+      else if (n == c && buff != "")
+	{
+	  v.push_back (buff);
+	  buff = "";
 	}
-	if (buff != "")
-		v.push_back(buff);
+    }
+  if (buff != "")
+    v.push_back (buff);
 
-	return v;
+  return v;
 }
 
-int main(int argc, char **argv) {
+int
+main (int argc, char **argv)
+{
 
-	XMLDocument doc;
-	doc.LoadFile("resources/scene.xml");
+  XMLDocument doc;
+  doc.LoadFile ("resources/scene.xml");
 
-	vector<string> modelos;
-	XMLElement* modelNode = doc.FirstChildElement("scene")->FirstChildElement(
-			"model");
+  vector<string> modelos;
+  XMLElement* modelNode = doc.FirstChildElement ("scene")->FirstChildElement (
+      "model");
 
-	for (auto crawl = modelNode; crawl != nullptr;
-			crawl = crawl->NextSiblingElement("model")) {
-		modelos.push_back(crawl->FirstAttribute()->Value());
-		cout << "Modelo:" << crawl->FirstAttribute()->Value() << endl;
+  for (auto crawl = modelNode; crawl != nullptr;
+      crawl = crawl->NextSiblingElement ("model"))
+    {
+      modelos.push_back (crawl->FirstAttribute ()->Value ());
+      cout << "Modelo:" << crawl->FirstAttribute ()->Value () << endl;
+    }
+
+  for (int i = 0; i < modelos.size (); i++)
+    {
+      string line;
+
+      vector<Triangle> lst;
+      ifstream myfile ("resources/" + modelos[i]);
+      if (myfile.is_open ())
+	{
+	  while (getline (myfile, line))
+	    {
+	      vector<string> v
+		{ explode (line, ';') };
+	      vector<string> p1
+		{ explode (v[0], ',') };
+	      vector<string> p2
+		{ explode (v[1], ',') };
+	      vector<string> p3
+		{ explode (v[2], ',') };
+
+	      Triangle t (atof (p1[0].c_str ()), atof (p1[1].c_str ()),
+			  atof (p1[2].c_str ()), atof (p2[0].c_str ()),
+			  atof (p2[1].c_str ()), atof (p2[2].c_str ()),
+			  atof (p3[0].c_str ()), atof (p3[1].c_str ()),
+			  atof (p3[2].c_str ()));
+
+	      lst.push_back (t);
+
+	    }
+	  myfile.close ();
+
+	  figures.push_back (lst);
+
 	}
 
-	/** TODO: Para já só está a ler da primeira entrada do ficheiro XML */
+      else
+	cout << "Unable to open file";
 
-	for (int i = 0; i < modelos.size(); i++) {
-		string line;
-
-		vector<Triangle> lst;
-		ifstream myfile("resources/" + modelos[i]);
-		if (myfile.is_open()) {
-			while (getline(myfile, line)) {
-				vector<string> v { explode(line, ';') };
-				vector<string> p1 { explode(v[0], ',') };
-				vector<string> p2 { explode(v[1], ',') };
-				vector<string> p3 { explode(v[2], ',') };
-
-				Triangle t(atof(p1[0].c_str()), atof(p1[1].c_str()),
-						atof(p1[2].c_str()), atof(p2[0].c_str()),
-						atof(p2[1].c_str()), atof(p2[2].c_str()),
-						atof(p3[0].c_str()), atof(p3[1].c_str()),
-						atof(p3[2].c_str()));
-
-				lst.push_back(t);
-
-			}
-			myfile.close();
-
-			figures.push_back(lst);
-
-		}
-
-		else
-			cout << "Unable to open file";
-
-	}
+    }
 
 // init GLUT and the window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(800, 800);
-	glutCreateWindow("CG@DI-UM");
+  glutInit (&argc, argv);
+  glutInitDisplayMode (GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowPosition (100, 100);
+  glutInitWindowSize (800, 800);
+  glutCreateWindow ("CG@DI-UM");
 
 // Required callback registry 
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
+  glutDisplayFunc (renderScene);
+  glutReshapeFunc (changeSize);
 
 // put here the registration of the keyboard callbacks
-	glutKeyboardFunc(keyboardR);
-	glutSpecialFunc(keyboardS);
+  glutKeyboardFunc (keyboardR);
+  glutSpecialFunc (keyboardS);
 
 // menu
-	glutCreateMenu(menu);
-	glutAddMenuEntry("GL POINT", 1);
-	glutAddMenuEntry("GL LINE", 2);
-	glutAddMenuEntry("GL FILL", 3);
+  glutCreateMenu (menu);
+  glutAddMenuEntry ("GL POINT", 1);
+  glutAddMenuEntry ("GL LINE", 2);
+  glutAddMenuEntry ("GL FILL", 3);
 
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
+  glutAttachMenu (GLUT_RIGHT_BUTTON);
 
 //  OpenGL settings
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
+  glEnable (GL_DEPTH_TEST);
+  glEnable (GL_CULL_FACE);
 
 // enter GLUT's main cycle
-	glutMainLoop();
+  glutMainLoop ();
 
-	return 1;
+  return 1;
 }
