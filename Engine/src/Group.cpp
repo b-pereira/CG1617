8d08142e23996f53d825911b0063eb74e2176d5a
_bpp <pereirabruno05@gmail.com>
2017-04-17 20:54:15 +0100

    Altera função de leitura e atualiza funções

--- Engine/src/Group.cpp
+++ Engine/src/Group.cpp
@@ -4,274 +4,210 @@
  *  Created on: 29/03/2017
  *      Author: bpereira
  */
-
+#include <sstream>
 #include "Group.h"
 #include "Constantes.h"
 
-const vector<string> explode(const string& s, const char& c)
-{
-    string buff { "" };
-    vector<string> v;
+const vector<string> explode(const string& s, const char& c) {
+	string buff { "" };
+	vector<string> v;
 
-    for (auto n : s)
-    {
-        if (n != c)
-            buff += n;
-        else if (n == c && buff != "")
-        {
-            v.push_back(buff);
-            buff = "";
-        }
-    }
-    if (buff != "")
-        v.push_back(buff);
+	for (auto n : s) {
+		if (n != c)
+			buff += n;
+		else if (n == c && buff != "") {
+			v.push_back(buff);
+			buff = "";
+		}
+	}
+	if (buff != "")
+		v.push_back(buff);
 
-    return v;
+	return v;
 }
 
-void drawElement(Modelos * models, Grupo * g)
-{
+void drawElement(Modelos * models, Grupo * g) {
 
-    glBegin(GL_TRIANGLES);
+	glBegin(GL_TRIANGLES);
 
-    for (int i = 0; i < g->modelos.size(); i++)
-    {
+	for (auto file : g->modelos) {
 
-        for (string file : g->modelos.at(i))
-        {
+		auto entry = models->figuras.find(file);
 
+		if (entry != models->figuras.end()) {
 
+			for (int i = 0; i < entry->second.size(); i += 3) {
+				glVertex3f(entry->second[i], entry->second[i + 1],
+						entry->second[i + 2]);
 
+			}
+		}
 
-            auto entry = models->figuras.find(file);
+	}
 
-            if(entry != models->figuras.end())
-            {
-                for (Triangle t : entry->second)
-                {
-
-                    Point3d a = t.getA();
-                    Point3d b = t.getB();
-                    Point3d c = t.getC();
-
-                    glVertex3f(a.getXCoord(), a.getYCoord(), a.getZCoord());
-
-                    glVertex3f(b.getXCoord(), b.getYCoord(), b.getZCoord());
-
-                    glVertex3f(c.getXCoord(), c.getYCoord(), c.getZCoord());
-
-
-                }
-            }
-
-
-
-
-
-        }
-    }
-
-    glEnd();
+	glEnd();
 
 }
 
-void lerFicheiro(Modelos * models, string file)
-{
+void lerFicheiro(Modelos * models, string file) {
 
-    string line;
+	string line;
 
-    vector<Triangle> lst;
-    ifstream myfile("resources/" + file);
-    if (myfile.is_open())
-    {
-        while (getline(myfile, line))
-        {
-            vector<string> v { explode(line, ';') };
-            vector<string> p1 { explode(v[0], ',') };
-            vector<string> p2 { explode(v[1], ',') };
-            vector<string> p3 { explode(v[2], ',') };
+	vector<double> lst;
+	ifstream myfile("resources/" + file, ios::in);
+	if (myfile.is_open()) {
+		while (getline(myfile, line)) {
 
-            Triangle t(atof(p1[0].c_str()), atof(p1[1].c_str()),
-                       atof(p1[2].c_str()), atof(p2[0].c_str()),
-                       atof(p2[1].c_str()), atof(p2[2].c_str()),
-                       atof(p3[0].c_str()), atof(p3[1].c_str()),
-                       atof(p3[2].c_str()));
+			std::stringstream is(line);
+			double x, y, z;
+			is >> x;
+			is >> y;
+			is >> z;
 
-            lst.push_back(t);
+			lst.push_back(x);
+			lst.push_back(y);
+			lst.push_back(z);
 
-        }
-        myfile.close();
+		}
+		myfile.close();
 
+		models->figuras.insert(std::pair<string, vector<double>>(file, lst));
 
+	}
 
-
-
-        models->figuras.insert ( std::pair<string, vector<Triangle>>(file, lst));
-
-
-
-    }
-
-    else
-        cout << "Unable to open file";
+	else
+		cout << "Unable to open file";
 
 }
 
-void readXMLFromRootElement(XMLElement * element, Modelos * models, Grupo * grupo)
-{
+void readXMLFromRootElement(XMLElement * element, Modelos * models,
+		Grupo * grupo) {
 
-    if (element == nullptr)
-        return;
+	if (element == nullptr)
+		return;
 
+	/**
+	 * Para cada irmão do elemento inicial: procurar atributos
+	 */
+	for (element = element->FirstChildElement(); element;
+			element = element->NextSiblingElement()) {
 
+		string name(element->Name());
 
-    /**
-     * Para cada irmão do elemento inicial: procurar atributos
-     */
-    for (element = element->FirstChildElement(); element;
-            element = element->NextSiblingElement())
-    {
+		Transformation * tr = 0;
 
-        string name(element->Name());
+		if ((name.compare(TRANSLATE) == 0) || (name.compare(SCALE) == 0)) {
+			double x, y, z;
 
-        Transformation * tr = 0;
+			x = (element->Attribute(X)) ? atof(element->Attribute(X)) : 0;
+			y = (element->Attribute(Y)) ? atof(element->Attribute(Y)) : 0;
+			z = (element->Attribute(Z)) ? atof(element->Attribute(Y)) : 0;
 
-        if ((name.compare(TRANSLATE) == 0) || (name.compare(SCALE) == 0))
-        {
-            double x, y, z;
+			if (name.compare(TRANSLATE) == 0) {
+				/**
+				 * Criar Translação
+				 */
+				Translation * t = new Translation(x, y, z);
 
-            x = (element->Attribute(X)) ? atof(element->Attribute(X)) : 0;
-            y = (element->Attribute(Y)) ? atof(element->Attribute(Y)) : 0;
-            z = (element->Attribute(Z)) ? atof(element->Attribute(Y)) : 0;
+				grupo->transformations.push_back(t);
 
-            if (name.compare(TRANSLATE) == 0)
-            {
-                /**
-                 * Criar Translação
-                 */
-                Translation * t = new Translation(x, y, z);
+			} else {
+				/**
+				 * Criar Escala
+				 */
+				Scale * s = new Scale(x, y, z);
 
-                grupo->transformations.push_back(t);
+				grupo->transformations.push_back(s);
 
-            }
-            else
-            {
-                /**
-                 * Criar Escala
-                 */
-                Scale * s = new Scale(x, y, z);
+			}
 
-                grupo->transformations.push_back(s);
+		} else if (name.compare(ROTATE) == 0) {
+			double axis_x, axis_y, axis_z, angle;
 
-            }
+			angle = (element->Attribute(ANGLE)) ?
+					atof(element->Attribute(ANGLE)) : 0;
 
-        }
-        else if (name.compare(ROTATE) == 0)
-        {
-            double axis_x, axis_y, axis_z, angle;
+			axis_x =
+					(element->Attribute(AXIS_X)) ?
+							atof(element->Attribute(AXIS_X)) : 0;
+			axis_y =
+					(element->Attribute(AXIS_Y)) ?
+							atof(element->Attribute(AXIS_Y)) : 0;
+			axis_z =
+					(element->Attribute(AXIS_Z)) ?
+							atof(element->Attribute(AXIS_Z)) : 0;
 
-            angle = (element->Attribute(ANGLE)) ?
-                    atof(element->Attribute(ANGLE)) : 0;
+			/**
+			 * Criar Rotação
+			 */
+			Rotation * r = new Rotation(angle, axis_x, axis_y, axis_z);
 
-            axis_x =
-                (element->Attribute(AXIS_X)) ?
-                atof(element->Attribute(AXIS_X)) : 0;
-            axis_y =
-                (element->Attribute(AXIS_Y)) ?
-                atof(element->Attribute(AXIS_Y)) : 0;
-            axis_z =
-                (element->Attribute(AXIS_Z)) ?
-                atof(element->Attribute(AXIS_Z)) : 0;
+			grupo->transformations.push_back(r);
 
-            /**
-             * Criar Rotação
-             */
-            Rotation * r = new Rotation(angle, axis_x, axis_y, axis_z);
+		} else if (name.compare(MODELS) == 0) {
 
-            grupo->transformations.push_back(r);
+			/**
+			 * Iterar sobre os ficheiros de modelos
+			 */
+			for (auto crawl = element->FirstChildElement(MODEL);
+					crawl != nullptr;
+					crawl = crawl->NextSiblingElement(MODEL)) {
 
-        }
-        else if (name.compare(MODELS) == 0)
-        {
+				string modelo(crawl->Attribute(FILE_D));
 
-            vector<string> lst;
+				grupo->modelos.push_back(modelo);
 
-            /**
-             * Iterar sobre os ficheiros de modelos
-             */
-            for (auto crawl = element->FirstChildElement(MODEL);
-                    crawl != nullptr;
-                    crawl = crawl->NextSiblingElement(MODEL))
-            {
+				lerFicheiro(models, modelo);
 
-                string modelo(crawl->Attribute(FILE_D));
+			}
 
-                lst.push_back(modelo);
+		} else if (name.compare(GROUP) == 0) {
 
-                lerFicheiro(models, modelo);
+			Grupo * novo_grupo = new Grupo;
+			grupo->filhos.push_back(novo_grupo);
+			readXMLFromRootElement(element, models, novo_grupo);
 
-            }
-            grupo->modelos.push_back(lst);
+		}
 
-        }
-        else if (name.compare(GROUP) == 0)
-        {
-
-
-
-            Grupo * novo_grupo = new Grupo;
-            grupo->filhos.push_back(novo_grupo);
-            readXMLFromRootElement(element,models,  novo_grupo);
-
-        }
-
-    }
+	}
 
 }
 
-Modelos * readXMLDoc(const char * path)
-{
+Modelos * readXMLDoc(const char * path) {
 
-    XMLDocument doc;
-    doc.LoadFile(path);
+	XMLDocument doc;
+	doc.LoadFile(path);
 
-    XMLElement* modelNode = doc.FirstChildElement(SCENE);
-    Modelos *  models = new Modelos;
-    models->g = new Grupo;
+	XMLElement* modelNode = doc.FirstChildElement(SCENE);
+	Modelos * models = new Modelos;
+	models->g = new Grupo;
 
-    readXMLFromRootElement(modelNode, models,  models->g);
+	readXMLFromRootElement(modelNode, models, models->g);
 
-    return models;
-
-}
-
-
-void traverseTree(Modelos * models, Grupo *grupo)
-{
-
-
-    glPushMatrix();
-    cout << "PUSH" << endl;
-
-    for (auto transformation : grupo->transformations)
-    {
-
-        transformation ->applyTransformation();
-
-    }
-
-    drawElement(models, grupo);
-
-    for (int i = 0; i < grupo->filhos.size(); i++)
-    {
-
-        traverseTree(models, grupo->filhos[i]);
-        glPopMatrix();
-        cout << "POP" << endl;
-
-    }
+	return models;
 
 }
 
+void traverseTree(Modelos * models, Grupo *grupo) {
 
+	glPushMatrix();
+	cout << "PUSH" << endl;
+
+	for (auto transformation : grupo->transformations) {
+
+		transformation->applyTransformation();
+
+	}
+
+	drawElement(models, grupo);
+
+	for (int i = 0; i < grupo->filhos.size(); i++) {
+
+		traverseTree(models, grupo->filhos[i]);
+		glPopMatrix();
+		cout << "POP" << endl;
+
+	}
+
+}
 
