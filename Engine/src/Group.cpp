4fcefdee2969f46b5086dfec50f61e75fb023faa
_bpp <pereirabruno05@gmail.com>
2017-04-17 21:15:22 +0100

    Remove função de parsing explode

--- Engine/src/Group.cpp
+++ Engine/src/Group.cpp
@@ -8,23 +8,7 @@
 #include "Group.h"
 #include "Constantes.h"
 
-const vector<string> explode(const string& s, const char& c) {
-	string buff { "" };
-	vector<string> v;
 
-	for (auto n : s) {
-		if (n != c)
-			buff += n;
-		else if (n == c && buff != "") {
-			v.push_back(buff);
-			buff = "";
-		}
-	}
-	if (buff != "")
-		v.push_back(buff);
-
-	return v;
-}
 
 void drawElement(Models * models, Group * g) {
 
